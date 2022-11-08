3eb4360da47e8c6cf900fbe0bc7d73310c81f478
TomOak <tom@tomduchene.fr>
2022-11-08 16:34:09 +0100

    File Git Locks window under Tactical Adventures

--- Editor/Scripts/GitLocksDisplay.cs
+++ Editor/Scripts/GitLocksDisplay.cs
@@ -34,7 +34,7 @@ public class GitLocksDisplay : EditorWindow
         EditorApplication.hierarchyWindowItemOnGUI += DrawHierarchyLocks;
     }
 
-    [MenuItem("Window/Git Locks")]
+    [MenuItem("Window/Tactical Adventures/Git Locks")]
     public static void ShowWindow()
     {
         // Show existing window instance. If one doesn't exist, make one.
